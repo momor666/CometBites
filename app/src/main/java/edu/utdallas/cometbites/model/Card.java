6d5ae46875d5513ba4ed272f4a158c50a7ed9deb
twinklesharma1311 <twinklesharma1311@gmail.com>
2017-02-04 19:08:39 -0600

    Moved files to subfolder.

--- app/src/main/java/edu/utdallas/cometbites/model/Card.java
+++
@@ -1,7 +0,0 @@
-package edu.utdallas.cometbites.model;
-
-public abstract class Card {
-    private String cardName;
-
-    abstract int getNumber();
-}
--- app/src/main/java/edu/utdallas/cometbites/services/IExternalPayment.java
+++
@@ -1,6 +0,0 @@
-package edu.utdallas.cometbites.services;
-
-public interface IExternalPayment {
-    boolean authorize(int cardNumber);
-    boolean charge(int cardNumber, double amount);
-}
